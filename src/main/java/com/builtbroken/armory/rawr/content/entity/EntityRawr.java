21dceec0ff7da1cc260b77bf34a7d439b7accde8
Robert WC Seifert <rseifert.phone@gmail.com>
2018-10-13 18:24:03 -0400

    Added raytrace attack

--- src/main/java/com/builtbroken/armory/rawr/content/entity/EntityRawr.java
+++ src/main/java/com/builtbroken/armory/rawr/content/entity/EntityRawr.java
@@ -3,10 +3,7 @@ package com.builtbroken.armory.rawr.content.entity;
 import com.builtbroken.armory.rawr.content.entity.ai.TaskAttackWhenHarmed;
 import com.builtbroken.armory.rawr.content.entity.ai.TaskFindTarget;
 import com.google.common.base.Optional;
-import net.minecraft.entity.EntityCreature;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.IRangedAttackMob;
-import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.EntityAIAttackRanged;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
@@ -15,16 +12,21 @@ import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.pathfinding.PathNodeType;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 10/5/2018.
  */
-public class EntityRawr extends EntityCreature implements IRangedAttackMob
+public class EntityRawr extends EntityCreature implements IRangedAttackMob, IEntityOwnable
 {
     protected static final DataParameter<Optional<UUID>> OWNER_UNIQUE_ID = EntityDataManager.<Optional<UUID>>createKey(EntityRawr.class, DataSerializers.OPTIONAL_UNIQUE_ID);
 
@@ -125,7 +127,91 @@ public class EntityRawr extends EntityCreature implements IRangedAttackMob
     @Override
     public void attackEntityWithRangedAttack(EntityLivingBase target, float distanceFactor)
     {
+        Vec3d start = new Vec3d(this.posX, this.posY + (double) this.getEyeHeight(), this.posZ);
+        Vec3d end = new Vec3d(target.posX, target.posY + (double) target.getEyeHeight(), target.posZ);
 
+        //Check if we don't hit a block between us and the target
+        RayTraceResult result = this.world.rayTraceBlocks(start, end, false, true, false);
+        if (result == null)
+        {
+            Entity hit = findEntityOnPath(start, end);
+            if (hit != null)
+            {
+                hit.attackEntityFrom(DamageSource.causeIndirectDamage(this, this), 4); //TODO make custom damage source and create a method for getting damage
+            }
+        }
+
+        //TODO send packet to client to spawn effects
+        //TODO send packet on hit to spawn effects
+        //TODO play audio
+    }
+
+    @Nullable
+    protected Entity findEntityOnPath(Vec3d start, Vec3d end)
+    {
+        Entity result = null;
+
+        //Get all entities in zone of hit
+        List<Entity> list = this.world.getEntitiesInAABBexcluding(this, new AxisAlignedBB(start, end).grow(1.0D), null);
+        double d0 = 0.0D;
+
+        for (Entity entity : list)
+        {
+            if (entity != this && canHitEntity(entity))
+            {
+                //Calculate impact of bounding box
+                AxisAlignedBB axisalignedbb = entity.getEntityBoundingBox().grow(0.30000001192092896D); //TODO see what this magic number is used for
+                RayTraceResult raytraceresult = axisalignedbb.calculateIntercept(start, end);
+
+                if (raytraceresult != null)
+                {
+                    //Find closest hit point
+                    double distance = start.squareDistanceTo(raytraceresult.hitVec);
+                    if (distance < d0 || d0 == 0.0D)
+                    {
+                        result = entity;
+                        d0 = distance;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Checks if the RAWR's weapon can hit the entity. This
+     * allows for projectiles to pass through targets blocking
+     * attacks to create a more enjoyable experience.
+     *
+     * @param entity - entity hit
+     * @return true to allow the hit or pass through to next entity
+     */
+    protected boolean canHitEntity(Entity entity)
+    {
+        //TODO add support for Armory FoF system
+        //TODO add config to enable/disable friendly fire
+        if (entity instanceof IEntityOwnable)
+        {
+            return ((IEntityOwnable) entity).getOwnerId() != getOwnerId();
+        }
+        else if (entity instanceof EntityPlayer)
+        {
+            EntityPlayer player = ((EntityPlayer) entity);
+            if (player.getGameProfile().getId() == getOwnerId())
+            {
+                return false;
+            }
+            else if (player.isCreative() || player.isSpectator())
+            {
+                return false;
+            }
+        }
+        else if (entity.getTeam() != null && entity.getTeam().isSameTeam(getTeam()))
+        {
+            return entity.getTeam().getAllowFriendlyFire();
+        }
+        return true;
     }
 
     @Override
